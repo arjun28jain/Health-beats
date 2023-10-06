64cc6ee15495864bb1cefb4e69c52dd7287337bf
arjun28jain <arjunjain2892003@gmail.com>
2023-10-07 03:48:38 +0530

    Plugins

--- src/pages/Desktop1.js
+++ src/pages/Desktop1.js
@@ -2,7 +2,6 @@ import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Desktop1.css";
 import eSanjeevniLogo from './eSanjeevaniLogo.png';
-import arrow from './arrow.png'
 
 const Desktop1 = () => {
   const navigate = useNavigate();
@@ -13,13 +12,11 @@ const Desktop1 = () => {
 
   return (
     <div className="desktop-1">
-       {/* onClick={onDesktop1Click} */}
        <img src={eSanjeevniLogo} style={{width:'200px', height:'60px', position:'relative',left:'1300px',zIndex:'1',top:'20px'}}/>
-       <div> <button onClick={onDesktop1Click} style={{width:'100px', height:'100px', position:'relative',left:'1400px', top:'550px', backgroundColor:'#7dd588', border:'0px'}}> <img src={arrow} style={{width:'90px', height:'90px'}}/> </button></div>
        
       <div className="desktop-1-child"/>
       <div className="desktop-1-item" />
-      <img className="lg1-1-icon" alt="" src="/lg1-1@2x.png" />
+       <img className="lg1-1-icon" alt="" src="/lg1-1@2x.png" />
       <div className="desktop-1-inner" />
       <div className="welcome-to-swasthya-container">
         <p className="welcome-to-main" style={{position:'absolute', top:'-170px', left:'-125px', width:'400px'}}>Welcome to,</p>
@@ -29,11 +26,14 @@ const Desktop1 = () => {
         <p className="welcome-to">Place your thumbprint</p>
         <p className="welcome-to"> to</p>
       </div>
+      <button onClick={onDesktop1Click} >
       <img
         className="background-copy-1"
         alt=""
         src="/background-copy-1@2x.png"
       />
+      </button>
+
       <div className="get-started">Get Started</div>
       <div className="div" style={{position: 'absolute',letterSpacing:'0.05em', left:'13px', fontSize:'25px', left:'600px', top:'690px'}}> “आरोग्यं परमं भाग्यं स्वास्थ्यं सर्वार्थसाधनम्” </div>
     </div>
--- src/pages/Desktop2.js
+++ src/pages/Desktop2.js
@@ -3,6 +3,9 @@ import { useNavigate } from "react-router-dom";
 import Userimage from "../components/Userimage";
 import RightArrow from "../components/RightArrow";
 import "./Desktop2.css";
+import oxygen from "./oxygen.png";
+import bloodpressure from "./bloodpressure.png";
+import temperature from "./temperature.png";
 
 const Desktop2 = () => {
   const navigate = useNavigate();
@@ -12,25 +15,38 @@ const Desktop2 = () => {
   }, [navigate]);
 
   return (
-    <div className="desktop-2" onClick={onDesktop2ContainerClick}>
-      <img className="desktop-2-child" alt="" src="/polygon-1.svg" />
+    <div className="desktop-2" >
+
+      <div className="temperature" style={{width:'230px', height:'310px', position:'absolute', zIndex:'2', left:'720px', top:'270px'}}>
+        <img src={temperature} style={{width:'170px', height:'175px'}}></img>
+        <p style={{textAlign:'center', fontSize:'30px', position:'relative', top:'-25px', fontWeight:'bold', color:'black'}}>TEMPERATURE<br/> 37 C</p>
+      </div>
+        
+      <div className="bloodpressure" style={{width:'235px', height:'310px', position:'absolute', zIndex:'2', left:'970px', top:'270px'}}>
+        <img src={bloodpressure} style={{width:'170px', height:'175px', position:'relative', left:'60px'}}></img>
+        <p style={{textAlign:'center', fontSize:'30px', position:'relative', top:'-25px', fontWeight:'bold', color:'black'}}>BLOOD PRESSURE <br/> 120/80</p>
+      </div>
+
+      <div className="oxygen" style={{width:'280px', height:'310px', position:'absolute', zIndex:'2', left:'1220px', top:'270px'}}>
+        <img src={oxygen} style={{width:'170px', height:'175px', position:'relative', left:'60px'}}></img>
+        <p style={{textAlign:'center', fontSize:'30px', position:'relative', top:'-25px', fontWeight:'bold', color:'black'}}>OXYGEN LEVEL <br/> 96</p>
+      </div>
+
+      <img className="desktop-2-child" onClick={onDesktop2ContainerClick} alt="" src="/polygon-1.svg" style={{width:'550px', height:'550px', position:'relative', left:'110px', top:'175px'}}/>
       <Userimage />
       <div className="desktop-2-item" />
       <div className="desktop-2-inner" />
-      <img className="lg1-1-icon1" alt="" src="/lg1-11@2x.png" />
-      <div className="arjun-jain-male-container">
-        <p className="arjun-jain"> Arjun Jain</p>
-        <p className="arjun-jain"> Male</p>
-        <p className="yrs">
-          <span className="span">{`    `}</span>
-          <span className="span1">{` `}</span>
-          <span className="span">{`21 yrs `}</span>
+     <div style={{width:'220px', height:'220px'}}> <img className="lg1-1-icon1" alt="" src="/lg1-11@2x.png" style={{width:'150px', height:'150px', position:'absolute', zIndex:'2', top:'px', left:'1350px'}} /> ;</div> 
+      <div className="male-container" style={{width:'350px', height:'300px', display:'flex', flexDirection:'column', position:'absolute', left:'205px', top:'370px'}}>
+        <p className="arjun-jain" style={{textAlign:'center'}}>ARJUN JAIN</p>
+        <p className="gender" style={{textAlign:'center'}}>Male</p>
+        <p className="age" style={{margin:'0px',textAlign:'center'}}>
+          <span className="span">21 years</span>
         </p>
-        <p className="p"> 93******56</p>
+        <p className="p" style={{textAlign:'center', marginTop:'10px'}}>93******56</p>
       </div>
-      <b className="user-details">User Details:</b>
-      <RightArrow />
-      <img className="polygon-icon" alt="" src="/polygon-11.svg" />
+      <b className="user-details">USER DETAILS:</b>
+      <img className="polygon-icon" src="/polygon-11.svg" style={{width:'200px', height:'200px', position:'absolute', left:'275px'}} />
     </div>
   );
 };
